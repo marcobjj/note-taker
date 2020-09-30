255348f7653c5a88e8581825073785bcace69e33
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-09-29 21:28:07 -0700

    adds post and html

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+
 const PORT = process.env.PORT || 3001;
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// fetch the static html pages
+app.use(express.static('public'));
+
 
 const notes = require('./db/db.json');
 
@@ -14,12 +26,29 @@ app.get('/api/notes', (req, res) => {
  
   });
 
-console.log(notes.join());
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    // add note to json file and notes array in this function
+    const note = createNewNote(req.body, notes);
+  
+    res.json(note);
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
 
 
-app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
 
 const filterByQuery = (query,results) => {
     filteredResults = results;
@@ -28,3 +57,13 @@ const filterByQuery = (query,results) => {
 
     return filteredResults;
 }
+
+const createNewNote = (body, notesArray) => {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+      );
+    return note;
+  }
